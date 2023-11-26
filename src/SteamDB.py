41d39b7c1886d3f73d09e58379d42301e4e1eadf
Aryaman Gupta <96819393+AGlade7@users.noreply.github.com>
2023-11-26 19:12:44 +0530

    Changed path according to Git directory

--- src/SteamDB.py
+++ src/SteamDB.py
@@ -3,7 +3,7 @@ import psycopg2
 conn = psycopg2.connect("host=localhost dbname=steamdb port=5432 user=postgres")
 cur = conn.cursor()
 
-makeDBPath = './Code/DB/makeDB.sql'
+makeDBPath = '../create_database/makeDB.sql'
 with open(makeDBPath, 'r') as sql_file:
     sql_query = sql_file.read()
 
