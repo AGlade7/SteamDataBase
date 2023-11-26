5432290c694bce10d88c9da34dcb56423f15a62c
Sarvottam <sarvottamswaroop100@gmail.com>
2023-11-26 17:28:27 +0530

    Changed ER

--- er/er.js
+++ er/er.js
@@ -10,8 +10,8 @@ erDiagram
 	Resides || -- || User_Region : _
 
 	%%Game Status of review by Steam
-	UnderReview_WE || -- || GameStatus_IR : _
-	GameStatus_IR || -- || Game : _
+	%%UnderReview_WE || -- || GameStatus_IR : _
+	%%GameStatus_IR || -- || Game : _
 
 	%%Games published by GPC
 	%% GPC = GamePublishingCompany
@@ -24,25 +24,38 @@ erDiagram
 	AvailabeIn_IR || -- |{ Game_Region: _
 	AvailabeIn_IR || -- |{ Region: _
 
+	%%GameGenre
 	Game || -- || BelongsTo: _
 	BelongsTo|| -- o{ Genre: _
 	BelongsTo|| -- o{ Game_Genre: _
 
-	UnderReview_WE || -- || IsAvailable : _
-	IsAvailable || -- || AvailableGames : _
+
+	%%UnderReview_WE || -- || IsAvailable : _
+	%%IsAvailable || -- || AvailableGames : _
+	IsAvailable  || -- || GPC: _
+	IsAvailable  || -- || AvailableGames: _
 	Game || -- || IsAvailable : _
+	%%Game Review
 	User || -- || Reviewed : _
 	Game || -- || Reviewed : _
 	Review || -- || Reviewed : _
 
+	%%Game supported langs
 	Game || -- || Supports : _
-	Supports || -- || Game_Language : _
-	Supports || -- || Language : _
+	Supports || -- |{ Game_Language : _
+	Supports || -- |{ Language : _
+
+	%%Languages a user speaks
+	User || -- || Speaks : _
+	Speaks || -- |{ User_Language : _
+	Speaks || -- |{ Language : _
+
+
+
 	User{
 		int User_ID PK
 		varchar(255) Username
 		varchar(255) Password
-		varchar(255) Language
 		int age
 		varchar(255) Email_ID
 	}
@@ -66,7 +79,7 @@ erDiagram
 		int GPC_ID FK
 		date Game_Release_Date
 		int Age_Limit
-		varchar(10) status
+		%%varchar(10) status
 		%%decimal(10)(2) Price
 		%%varchar(50) Language
 		%%int GPC_ID FK
@@ -85,11 +98,13 @@ erDiagram
 		int RegionID FK
 		varchar(255) Game_Name
 	}
-	UnderReview_WE{
-		varchar(10) status
-	}
+	%%UnderReview_WE{
+	%%	varchar(10) status
+	%%}
 	AvailableGames{
-		int Game_ID FK
+		int Game_ID FK PK
+		int GPC_ID FK
+		varchar(255) GPC_Name
 		varchar(255) Game_Name
 
 	}
@@ -115,5 +130,9 @@ erDiagram
 	Game_Language{
 		int lang_id FK
 		int game_id FK
+	}
+	User_Language{
+		int lang_id FK
+		int user_id FK
 	}
 
